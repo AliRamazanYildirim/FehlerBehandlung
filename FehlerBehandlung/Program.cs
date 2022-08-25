6f664632645730b1c6535808a6270b36f423e989
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-08-25 16:40:15 +0300

    StatusCodePages()

--- FehlerBehandlung/Program.cs
+++ FehlerBehandlung/Program.cs
@@ -11,10 +11,19 @@ if (!app.Environment.IsDevelopment())
     //Mit UseDeveloperExceptionPage können wir auch zusätzliche Fehler sehen.
     app.UseDeveloperExceptionPage();
 
-    
+    //app.UseStatusCodePages("text/plain", "Es gibt Fehler. StatusCode:{0}");
+
+    app.UseStatusCodePages(async statusCodeContext =>
+    {
+        statusCodeContext.HttpContext.Response.ContentType = "text/plain";
+        await statusCodeContext.HttpContext.Response.WriteAsync(
+            $"Es gibt Fehler. StatusCode:{statusCodeContext.HttpContext.Response.StatusCode}");
+    });
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 //Ich habe diese Fehlermeldung aus dem if-Blog entfernt, da ich möchte,
 //dass sie außerhalb der Entwicklungsumgebung ausgeführt wird.
 app.UseExceptionHandler("/Home/Error");
